5200407fa24c4fc281d17ac983d64a618e7c3d12
AkshayM2001 <akshaymatale2001@gmail.com>
2024-05-29 23:16:11 +0530

    render 1.1

--- ChatBotAI/settings.py
+++ ChatBotAI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = api_key=os.getenv('api-key2')
+SECRET_KEY = "django-insecure--n)-!jrjnf6_#7l6f#m@071(h5t-e9*lasi74+)%m#v_d4f2=b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
