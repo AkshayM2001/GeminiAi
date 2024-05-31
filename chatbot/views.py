b45c5cd7215a55a5454cd1215ca080620221ab67
AkshayM2001 <akshaymatale2001@gmail.com>
2024-05-31 18:07:59 +0530

    Fix - no reverse match at login page

--- chatbot/views.py
+++ chatbot/views.py
@@ -32,7 +32,7 @@ def login(request):
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('chatbot')
+            return redirect('home')
         else:
             error_message = 'Invalid username or password'
             return render(request, 'login.html', {
